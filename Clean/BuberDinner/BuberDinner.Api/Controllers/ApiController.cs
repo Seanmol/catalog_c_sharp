dfefdd023c5393439da97a5c27d89a8c93e88bb6
Seanmol <seanmotsi@outlook.com>
2023-04-23 23:44:48 +0200

    FluentValidation

--- Clean/BuberDinner/BuberDinner.Api/Controllers/ApiController.cs
+++ Clean/BuberDinner/BuberDinner.Api/Controllers/ApiController.cs
@@ -1,23 +1,51 @@
 using BuberDinner.Api.Common.Http;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberDinner.Api.Controller;
 
 [ApiController]
 public class ApiController: ControllerBase
 {
+    
     protected IActionResult Problem(List<Error> errors)
     {
+        if(errors.Count == 0)
+        {
+            return Problem();
+        }
+
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
         HttpContext.Items[HttpContextItemKeys.Errors] = errors;
-        var firstError = errors[0];
-        var statusCode = firstError.Type switch
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
         {
             ErrorType.Conflict => StatusCodes.Status409Conflict,
             ErrorType.NotFound => StatusCodes.Status404NotFound,
             ErrorType.Validation => StatusCodes.Status400BadRequest,
             _ => StatusCodes.Status500InternalServerError
         };
-        return Problem(statusCode: statusCode, detail: firstError.Description);
+        return Problem(statusCode: statusCode, detail: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(
+                error.Code,
+                error.Description);
+        }
+        return ValidationProblem(modelStateDictionary);
     }
 }
--- Clean/BuberDinner/BuberDinner.Application/DependencyInjection.cs
+++ Clean/BuberDinner/BuberDinner.Application/DependencyInjection.cs
@@ -1,8 +1,12 @@
 
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Behaviors;
+using ErrorOr;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-
-
+using FluentValidation;
+using System.Reflection;
 
 namespace BuberDinner.Application;
 public static class DependencyInjection
@@ -14,7 +18,11 @@ public static class DependencyInjection
                             {
                                 cfg.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly, typeof(DependencyInjection).Assembly);
                             });
-        
+
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
